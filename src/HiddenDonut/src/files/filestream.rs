d9b02bd191db2316f23ae7c359233dd1048e2066
golle <jens5@kth.se>
2019-03-28 10:31:59 +0100

    Delete rust project, going with a .net core implementation

--- src/HiddenDonut/src/files/filestream.rs
+++
@@ -1,7 +0,0 @@
-use std::fs::File;
-
-pub trait FileStream {
-    fn open(&self, filename: &str) -> Result<File, &'static str>;
-    fn create(&self, filename: &str) -> Result<File, &'static str>;
-}
-
--- src/HiddenDonut/src/files/mod.rs
+++
@@ -1,6 +0,0 @@
-mod filestream;
-mod simple_file_stream;
-
-pub use filestream::FileStream;
-pub use simple_file_stream::SimpleFileStream;
-
